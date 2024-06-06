048a6edcd6ed9fee3cf0b87fdb86e9de53509ec6
AngelFernandoPacsiPonce <angel.pacsi@ucsp.edu.pe>
2024-06-05 22:47:42 -0500

    Nivel 2 MAPA CARGADO

--- PACMAN-CPP/PACMAN-CPP/Nivel2.cpp
+++ PACMAN-CPP/PACMAN-CPP/Nivel2.cpp
@@ -1,28 +1,107 @@
 #include "Nivel2.h"
 #include <iostream>
 
-Nivel2::Nivel2(sf::RenderWindow& ventana, float ancho, float alto) : ventana(ventana) {
+Nivel2::Nivel2(sf::RenderWindow& ventana, float ancho, float alto)
+    : ventana(ventana), jugador(ancho / 2, alto / 2, 1.8f), fantasmaRojo("Nivel2/fantasma.png", ancho / 4, alto / 4, 1.5f) {
     if (!fondoTextura.loadFromFile("Nivel2/FondoNivel2.png")) {
         std::cerr << "Error al cargar la imagen de fondo." << std::endl;
-        
     }
 
     fondoSprite.setTexture(fondoTextura);
     fondoSprite.setScale(ancho / fondoSprite.getGlobalBounds().width, alto / fondoSprite.getGlobalBounds().height);
 
+    inicializarMapa();
+
     musicaNivel2.cargarMusicaNivel2();
 }
 
+void Nivel2::inicializarMapa() {
+    mapa = new int* [altoMapa];
+    for (int i = 0; i < altoMapa; ++i) {
+        mapa[i] = new int[anchoMapa];
+    }
+
+    int miMapa[13][32] = {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1},
+        {1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1},
+        {1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1},
+        {1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1},
+        {1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1},
+        {1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1},
+        {1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1},
+        {1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1},
+        {1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    };
+
+    for (int i = 0; i < altoMapa; ++i) {
+        for (int j = 0; j < anchoMapa; ++j) {
+            mapa[i][j] = miMapa[i][j];
+        }
+    }
+}
+
 void Nivel2::mostrar() {
-    
-    ventana.draw(fondoSprite); 
-    musicaNivel2.reproducir(); 
+    anchoCelda = ventana.getSize().x / (anchoMapa * 1.8);
+    altoCelda = ventana.getSize().y / (altoMapa * 2.6);
+    posXInicio = (ventana.getSize().x - (anchoCelda * anchoMapa)) / 2;
+    posYInicio = (ventana.getSize().y - (altoCelda * altoMapa)) / 2;
+
+    ventana.draw(fondoSprite);
+
+    for (int i = 0; i < altoMapa; ++i) {
+        for (int j = 0; j < anchoMapa; ++j) {
+            if (mapa[i][j] == 1) {
+                sf::RectangleShape celda(sf::Vector2f(anchoCelda, altoCelda));
+                celda.setPosition(posXInicio + j * anchoCelda, posYInicio + i * altoCelda);
+                celda.setFillColor(sf::Color::Green);
+                ventana.draw(celda);
+            }
+        }
+    }
+
+    jugador.dibujar(ventana);
+    fantasmaRojo.dibujar(ventana);
+
+    musicaNivel2.reproducir();
 }
 
 void Nivel2::actualizar() {
-    
+    float deltaTime = 1.0f / 60.0f;
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+        jugador.setDireccion(sf::Vector2f(0, -1));
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+        jugador.setDireccion(sf::Vector2f(0, 1));
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+        jugador.setDireccion(sf::Vector2f(-1, 0));
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+        jugador.setDireccion(sf::Vector2f(1, 0));
+    }
+
+    jugador.mover(jugador.getDireccion(), mapa, anchoMapa, altoMapa, anchoCelda, altoCelda, posXInicio, posYInicio);
+    jugador.actualizarAnimacion(deltaTime);
+
+    fantasmaRojo.mover(mapa, anchoMapa, altoMapa, anchoCelda, altoCelda, posXInicio, posYInicio, jugador.getPosicion());
 }
 
 void Nivel2::manejarEventos() {
-   
+    // Manejo de eventos (actualmente vacío)
+}
+
+bool Nivel2::posicionValida(sf::Vector2f posicion) {
+    int fila = static_cast<int>((posicion.y - posYInicio) / altoCelda);
+    int columna = static_cast<int>((posicion.x - posXInicio) / anchoCelda);
+
+    if (fila >= 0 && fila < altoMapa && columna >= 0 && columna < anchoMapa) {
+        return mapa[fila][columna] == 0;
+    }
+
+    return false;
 }
--- PACMAN-CPP/PACMAN-CPP/Nivel2.h
+++ PACMAN-CPP/PACMAN-CPP/Nivel2.h
@@ -1,24 +1,33 @@
 #pragma once
-#ifndef NIVEL2_H
-#define NIVEL2_H
 
 #include <SFML/Graphics.hpp>
-#include "Musica.h" 
+#include "Jugador.h"
+#include "Musica.h"
+#include "FantasmaRojo.h"
 
 class Nivel2 {
 public:
     Nivel2(sf::RenderWindow& ventana, float ancho, float alto);
-
     void mostrar();
     void actualizar();
     void manejarEventos();
+    bool posicionValida(sf::Vector2f posicion);
 
 private:
     sf::RenderWindow& ventana;
-    Musica musicaNivel2; 
+    Musica musicaNivel2;
     sf::Texture fondoTextura;
     sf::Sprite fondoSprite;
-    
-};
+    Jugador jugador;
+    FantasmaRojo fantasmaRojo;
+    std::vector<sf::Sprite> objetos;
+    const int anchoMapa = 32;
+    const int altoMapa = 13;
+    int** mapa;
+    float anchoCelda;
+    float altoCelda;
+    float posXInicio;
+    float posYInicio;
 
-#endif
+    void inicializarMapa();
+};
